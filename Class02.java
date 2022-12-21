be03214f533cb95443004260fe828b8346e0bfc5
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-21 13:57:01 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -1,51 +1,14 @@
-#1
-import java.io.*;
+import java.util.Scanner;
+import java.lang.String;
 
-public class hw14_4 {
-	public static void main(String args[]) throws IOException {
-		char data[] = new char[128];
-		FileReader fr = new FileReader("C:\\Users\\WIN\\eclipse-workspace\\Java14\\src\\donkey.txt");
-
-		int num = fr.read(data);
-		String str = new String(data, 0, num);
-		System.out.println("Characters read= " + num);
-		System.out.println(str);
-
-		fr.close();
-	}
-}
-
-#2
-import java.io.*;
-
-public class hw14_5 {
-	public static void main(String args[]) throws IOException {
-		char data[] = new char[128];
-		FileReader fr = new FileReader("C:\\Users\\WIN\\eclipse-workspace\\Java14\\src\\donkey.txt");
-		fr.skip(9);
-		int num = fr.read(data);
-		String str = new String(data, 0, num);
-		System.out.println("Characters read= " + num);
-		System.out.println(str);
-		fr.close();
-	}
-}
-
-#3
-import java.io.*;
-
-public class hw14_6 {
+public class hw14_2 {
 	public static void main(String args[]) {
-		try {
-			char data[] = new char[128];
-			FileReader fr = new FileReader("C:\\Users\\WIN\\eclipse-workspace\\Java14\\src\\train.txt");
+		Scanner scn = new Scanner(System.in);
+		String str;
 
-			int num = fr.read(data);
-			String str = new String(data, 0, num);
-			System.out.println("Characters read= " + num);
-			System.out.println(str);
-			fr.close();
-		} catch (IOException e) {
-		}
+		System.out.print("輸入字串:");
+		str = scn.nextLine();
+
+		System.out.println("轉換成大寫: " + str.toUpperCase());
 	}
 }
