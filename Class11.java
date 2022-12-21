41837d978865aadb7ca39219aa68fab79384d1d1
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-21 13:58:56 +0800

    Create Class11.java

---
+++ Class11.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class hw14_11 {
+	public static void main(String args[]) throws IOException {
+		FileInputStream fi = new FileInputStream("C:\\Users\\WIN\\eclipse-workspace\\Java14\\src\\number.txt");
+		byte data[] = new byte[fi.available()];
+		int count = 0;
+
+		fi.read(data);
+		String str = new String(data);
+		System.out.println(str);
+
+		char arr[] = str.toCharArray();
+
+		for (int i = 0; i < arr.length; i++)
+			if (arr[i] == '\r')
+				count++;
+
+		System.out.println((count + 1) + " lines read");
+		fi.close();
+	}
+}
